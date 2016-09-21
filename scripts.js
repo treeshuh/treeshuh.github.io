c0337a51cd03baf5fea33bb3f29f15b7cc0b1948
treeshuh <tricia.shi@gmail.com>
2016-09-21 00:52:02 -0400

    base projects

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 5);
+}
+
+
+window.onload = function() {
+	document.getElementById("projectsLink").onclick = function(e) {
+		e.preventDefault();
+		scrollTo(document.body, window.innerHeight, 1000);
+	}	
+}
